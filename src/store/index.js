f7a3fa69469d1422e38b8174a67d5a443ef2f808
Marcel Souza <masr@ecomp.poli.br>
2019-12-04 18:00:47 -0300

    Created functions to add cards and lists, stopped implementation of draggable

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,115 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     todos: [],
-    newTodo: ""
+    newTodo: "",
+
+    lists: [
+      {
+        title: "Minha lista",
+        cards: [
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now()
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 1
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 2
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 3
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 4
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 5
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 6
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 7
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 8
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 9
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 10
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 11
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 12
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 13
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 14
+          },
+          {
+            title: "Teste",
+            description: "Card para teste",
+            comments: [],
+            id: Date.now() + 15
+          }
+        ],
+        id: Date.now()
+      }
+    ],
+    archivedLists: [],
+    newList: "",
+    newCard: ""
   },
   mutations: {
     GET_TODO(state, todo) {
@@ -35,6 +143,40 @@ export default new Vuex.Store({
     },
     CLEAR_TODO(state) {
       state.newTodo = "";
+    },
+
+    GET_LIST(state, list) {
+      state.newList = list;
+    },
+    ADD_LIST(state) {
+      state.lists.push({
+        title: state.newList,
+        cards: []
+      });
+    },
+    EDIT_LIST(state, list) {
+      let lists = state.lists;
+      lists.splice(lists.indexOf(list), 1);
+      state.lists = lists;
+      state.newList = list.title;
+    },
+    ARCHIVE_LIST(state, list) {
+      let lists = state.lists;
+      lists.splice(lists.indexOf(list), 1);
+      state.archivedLists.push(list);
+    },
+
+    GET_CARD(state, card) {
+      state.newCard = card;
+    },
+    ADD_CARD(state, cardInfo) {
+      state.lists
+        .find(list => list.id === cardInfo.listId)
+        .cards.push({
+          title: newCard,
+          description: null,
+          id: Date.now()
+        });
     }
   },
   actions: {
@@ -55,6 +197,16 @@ export default new Vuex.Store({
     },
     clearTodo({ commit }) {
       commit("CLEAR_TODO");
+    },
+
+    getList({ commit }, list) {
+      commit("GET_LIST", list);
+    },
+    addList({ commit }) {
+      commit("ADD_LIST");
+    },
+    archiveList({ commit }, list) {
+      commit("ARCHIVE_LIST", list);
     }
   },
   getters: {
@@ -66,7 +218,10 @@ export default new Vuex.Store({
     completedTodos: state =>
       state.todos.filter(todo => {
         return todo.completed;
-      })
+      }),
+
+    newList: state => state.newList,
+    lists: state => state.lists
   },
   modules: {}
 });
--- src/views/Trello.vue
+++ src/views/Trello.vue
@@ -3,123 +3,47 @@
     <AppHeader appName="Trello"></AppHeader>
     <AppContentBox>
       <div class="tile is-ancestor">
-        <div class="tile is-3 is-parent">
-          <div class="tile is-child box card-list">
+        <div v-for="list in lists" :key="list.id" class="tile is-3 is-parent">
+          <div class="tile is-child box card-list is-paddingless">
             <div class="subtitle">
-              <h2>Lista</h2>
+              <h2>{{list.title}}</h2>
             </div>
-            <div class="content">
+            <div class="content is-marginless">
               <ul>
                 <div>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
+                  <li v-for="card in list.cards" :key="card.id" class="box">{{card.title}}</li>
                 </div>
               </ul>
             </div>
+            <button class="button is-fullwidth is-grey">
+              <font-awesome-icon icon="plus" pull="left" :style="{color: 'white'}"></font-awesome-icon>Adicionar cartão
+            </button>
           </div>
         </div>
         <div class="tile is-3 is-parent">
-          <div class="tile is-child box card-list">
-            <div class="subtitle">
-              <h2>Lista</h2>
-            </div>
-            <div class="content">
-              <ul>
-                <div>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                </div>
-              </ul>
-            </div>
+          <div v-if="!createList" class="tile is-child">
+            <button @click="createList = !createList" class="button is-fullwidth is-grey">
+              <font-awesome-icon icon="plus" pull="left"></font-awesome-icon>Adicionar outra lista
+            </button>
           </div>
-        </div>
-        <div class="tile is-3 is-parent">
-          <div class="tile is-child box card-list">
-            <div class="subtitle">
-              <h2>Lista</h2>
-            </div>
+          <div v-else class="tile is-child box card-list" id="addList">
             <div class="content">
-              <ul>
-                <div>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
+              <div class="field">
+                <div class="control">
+                  <input
+                    type="text"
+                    class="input"
+                    :value="newList"
+                    @change="getList"
+                    placeholder="Insira o título da lista..."
+                  />
                 </div>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="tile is-3 is-parent">
-          <div class="tile is-child box card-list">
-            <div class="subtitle">
-              <h2>Lista</h2>
-            </div>
-            <div class="content">
-              <ul>
-                <div>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
+              </div>
+              <div class="field is-grouped">
+                <div class="control">
+                  <button @click="uncheckAddListButton" class="button is-success">Adicionar Lista</button>
                 </div>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="tile is-3 is-parent">
-          <div class="tile is-child box card-list">
-            <div class="subtitle">
-              <h2>Lista</h2>
-            </div>
-            <div class="content">
-              <ul>
-                <div>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                </div>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="tile is-3 is-parent">
-          <div class="tile is-child box card-list">
-            <div class="subtitle">
-              <h2>Lista</h2>
-            </div>
-            <div class="content">
-              <ul>
-                <div>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                  <li class="box">Teste</li>
-                </div>
-              </ul>
+              </div>
             </div>
           </div>
         </div>
@@ -132,11 +56,43 @@
 import AppBoard from "@/components/AppBoard.vue";
 import AppHeader from "@/components/AppHeader.vue";
 import AppContentBox from "@/components/AppContentBox.vue";
+import draggable from "vuedraggable";
+
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+library.add(faPlus);
+
 export default {
   components: {
     AppBoard,
     AppHeader,
     AppContentBox
+  },
+  data() {
+    return {
+      createCard: false,
+      createList: false
+    };
+  },
+  methods: {
+    getList(e) {
+      return this.$store.dispatch("getList", e.target.value);
+    },
+    addList() {
+      return this.$store.dispatch("addList");
+    },
+    uncheckAddListButton() {
+      this.addList();
+      this.createList = false;
+    }
+  },
+  computed: {
+    lists() {
+      return this.$store.getters.lists;
+    },
+    newList() {
+      return this.$store.getters.newList;
+    }
   }
 };
 </script>
@@ -146,25 +102,37 @@ export default {
   color: #363636 !important;
 }
 .card-list {
-  padding: 10px;
-  max-height: 70%;
+  height: fit-content;
+  background-color: aliceblue;
+}
+#addList {
+  height: fit-content;
 }
 .content {
-  max-height: 75%;
+  max-height: 100%;
   overflow-y: auto;
+  margin-right: 5px !important;
 }
 .content ul {
   margin: 5px 0;
+  max-height: 225px;
 }
 .subtitle {
-  padding: 5px 10px;
-  margin: 0 !important;
+  padding: 5px 20px 0;
+  margin: 5px 0 0 0 !important;
 }
 li.box {
-  margin: 0 2px;
+  margin: 5px 5px 5px 10px;
   padding: 10px;
 }
 li.box:not(:last-child) {
   margin-bottom: 10px;
+}
+button.is-grey {
+  background-color: darkgrey;
+  color: white !important;
+}
+.button svg path {
+  color: white !important;
 }
 </style>
